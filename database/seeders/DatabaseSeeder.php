e358d91a759811a71e3b7e064a8118687e4223eb
Eric Eckstein <eeckstein@ellipticworks.com>
2022-02-08 14:52:28 -0500

    BASELINE:   IDe-Helper Installed and Setup Laratrust installed with seeder and registration panel Must run php artisan migrate:fresh --seed to install again.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+
+        $this->call(LaratrustSeeder::class);
         // \App\Models\User::factory(10)->create();
     }
 }
