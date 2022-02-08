f1428715eada8b81d292a5a680b3becb18cfee30
Eric Eckstein <eeckstein@ellipticworks.com>
2022-02-08 16:34:05 -0500

    Changed config/app.php to add app_version and api_version Added BumblebeeController.php, AuthController.php and MeasurementController.php to App/Http/Controllers Added routes/api.php for the current bumblebee api

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Bumblebee;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class AuthController extends Controller
+{
+
+    public function logout(Request $request){
+//        auth()->user()->tokens()->
+        auth()->user()->tokens()->delete();
+
+        return response([
+            'message' => 'Logged Out'
+        ], 200);
+    }
+
+    public function login(Request $request)
+    {
+        $fields = $request->validate([
+            'remember_token' => 'required|string',
+            'api_password' => 'required|string'
+        ]);
+
+        // Check Key
+        $bumblebee = Bumblebee::where('remember_token', $fields['remember_token'])->first();
+
+        if (!$bumblebee || !Hash::check($fields['api_password'], $bumblebee->api_password)){
+            return response([
+                'message' => 'bad credentials'
+            ], 401);
+        }
+
+        $token = $bumblebee->createToken($bumblebee->serial_number.'_api')->plainTextToken;
+
+        $response = [
+            'bumblebee' => $bumblebee,
+            'token' => $token
+        ];
+
+        return response($response, 200);
+
+    }
+
+
+    public function register(Request $request)
+    {
+        $fields = $request->validate([
+            'serial_number' => 'required|string|match:bumblebees,serial_number',
+            'remember_token' => 'required|string|match:bumblebees,remember_token',
+            'api_password' => 'required|string'
+        ]);
+
+        $bumblebee = Bumblebee::create([
+            'serial_number' => $fields['serial_number'],
+            'remember_token' => $fields['remember_token'],
+            'api_password' => bcrypt($fields['api_password'])
+        ]);
+
+        $token = $bumblebee->createToken($fields['serial_number'].'_api')->plainTextToken;
+
+        $response = [
+            'bumblebee' => $bumblebee,
+            'token' => $token
+        ];
+
+        return response($response, 200);
+
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BumblebeeController;
+use App\Http\Controllers\MeasurementController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +17,49 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+/*
+ * Protected Routes
+ */
+Route::group(['middleware' => ['auth:sanctum']], function (){
+
+    Route::post('/measurement/post/', [MeasurementController::class, 'store']);
+    Route::get('/measurement/show/all', [MeasurementController::class, 'index']);
+    Route::get('/measurement/show/{id}', [MeasurementController::class, 'show']);
+    Route::get('/measurement/show/bumblebee/{id}', [MeasurementController::class, 'searchBumblebee']);
+    Route::get('/measurement/show/bumblebee/{id}/{metric}', [MeasurementController::class, 'searchBumblebeeMetric']);
+    Route::put('/measurement/update/{id}', [MeasurementController::class, 'update']);
+    Route::delete('/measurement/delete/{id}', [MeasurementController::class, 'destroy']);
+
+    Route::post('/calibration/post/', [MeasurementController::class, 'storeCalibration']);
+    Route::get('/calibration/show/all', [MeasurementController::class, 'indexCalibration']);
+    Route::get('/calibration/show/{id}', [MeasurementController::class, 'showCalibration']);
+    Route::get('/calibration/show/bumblebee/{id}', [MeasurementController::class, 'searchBumblebeeCalibration']);
+    Route::get('/calibration/show/bumblebee/{id}/{metric}', [MeasurementController::class, 'searchBumblebeeMetricCalibration']);
+    Route::delete('/calibration/delete/{id}', [MeasurementController::class, 'destroyCalibration']);
+
+    Route::get('/bumblebee/show/all', [BumblebeeController::class, 'index']);
+    Route::get('/bumblebee/show/{id}', [BumblebeeController::class, 'show']);
+    Route::post('/bumblebee/post/', [BumblebeeController::class, 'store']);
+    Route::put('/bumblebee/update/{id}', [BumblebeeController::class, 'update']);
+    Route::delete('/bumblebee/delete/{id}', [BumblebeeController::class, 'destroy']);
+
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+});
+
+/**
+ * Public Routes
+ */
+
+// Return Version of Bumblebee API
+Route::get('/version', function (){
+    return Response::json([
+        'application' => config('app.name'),
+        'data_model' => 'bumblebee',
+        'api_version' => config('app.api_version'),
+    ], 200);
 });
